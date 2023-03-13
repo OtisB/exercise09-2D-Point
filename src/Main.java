af7c5157e809fce24a944ce607b4224bf5e6ac93
Robin Schmidt <schmidt.rbn@gmail.com>
2023-03-13 09:05:01 +0100

    ADD Main and Point classes

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import vectorCalculation.Point;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Point p1 = new Point(3, 1);
+        Point p2 = new Point(2, 3);
+        Point p3 = new Point();
+
+        System.out.println(p1);
+
+    }
+}
